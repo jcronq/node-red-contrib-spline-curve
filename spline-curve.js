69435e8c2e151b644f1cc97a4ab6349757a2b4b6
jcronq <jcronq@gmail.com>
2020-01-05 18:57:37 +0000

    adding ability to store and retrieve values from different parts of msg

--- spline-curve.js
+++ spline-curve.js
@@ -6,9 +6,35 @@ module.exports = function(RED) {
 
         curve = new Curve(config);
 
+        const iterableAssignment = function(obj, keys, value){
+            if(keys.length == 1){
+                obj[keys[0]] = value;
+            }
+            else if(keys.length > 1){
+                const thisKey = keys[0];
+                keys.splice(0,1);
+                if(!obj[thisKey])
+                    obj[thisKey] = {};
+                iterableAssignment(obj[thisKey], keys, value);
+            }
+        }
+
         var node = this;
         node.on('input', function(msg){
-            msg.payload = curve.getValueAt(msg.payload);
+            console.log(config);
+            const input_key  = (config.input_key)?config.input_key:"payload";
+            const output_key = (config.output_key)?config.output_key:"payload";
+            const inputKeys  = input_key.split(".");
+            const outputKeys = output_key.split(".");
+
+            var inputValue = msg;
+            for( i=0; i < inputKeys.length; i++ ){
+                inputValue = inputValue[inputKeys[i]];
+            }
+
+            const result = curve.getValueAt(inputValue);
+            console.log(outputKeys);
+            iterableAssignment(msg, outputKeys, result);
             node.send(msg);
         });
     }
